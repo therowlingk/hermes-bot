1dd42c79e4e607c12cbf57500943d6f72c216731
Dimas Suprayoga <34300619+therowlingk@users.noreply.github.com>
2026-05-19 13:08:11 +0700

    Refactor bot token initialization and logging

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, {
   polling: true
 });
-
-console.log("Bot starting...");
 
 bot.on('message', (msg) => {
   console.log(msg.text);
 
-  if(msg.text === '/start'){
+  if (msg.text === '/start') {
     bot.sendMessage(msg.chat.id, 'Hermes Bot Online 🚀');
   }
 });
+
+console.log('Bot Running...');
